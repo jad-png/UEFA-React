5615baca3365d81dce0cd0545af6d03f2715532a
jad-png <j.belassiria@uca.ca.ma>
2025-04-23 11:31:40 +0100

    Implement fetchMatches function to retrieve data from API and handle loading and error states

--- src/stores/matchStore.jsx
+++ src/stores/matchStore.jsx
@@ -1,14 +1,17 @@
 import { create } from "zustand";
-
+import api from "../services/api";
 
 const useMatchStore = create((set) => ({
-    matches: [],
-    loading: false,
-    error: null,
-    fetchMatches: async () => {
-        set({ loading: true });
-        try {
-            const response = await 
-        }
+  matches: [],
+  loading: false,
+  error: null,
+  fetchMatches: async () => {
+    set({ loading: true });
+    try {
+      const response = await api.get("#");
+      set({ matches: response.data, error: null });
+    } catch (error) {
+      set({ error: error.message, loading: false });
     }
+  },
 }));
