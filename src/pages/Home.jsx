d5a057eeeb93c3b5802c6f14544ca4c21520a909
jad-png <j.belassiria@uca.ca.ma>
2025-04-23 11:56:40 +0100

    Add Home component and configure routing for MatchList

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { MatchDetails } from "../components/MatchDetails";
+import { MatchList } from "../components/MatchList";
+
+export function Home() {
+    return (
+        <div>
+            <MatchList />
+        </div>
+    );
+}
---
+++ src/routes/index.jsx
@@ -0,0 +1,8 @@
+import { MatchList } from "../components/MatchList";
+import mainLayout from "../layouts/mainLayout";
+
+const routes = [
+    { path: '/', component: MatchList, layout: mainLayout},
+];
+
+export default routes;
